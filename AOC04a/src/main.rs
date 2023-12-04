a48a52dd173b35f3bfe7c0fbb49d1da8c262717b
Signor-Koala <jeffrey.samuel@protonmail.com>
2023-12-04 11:42:35 +0530

    Day 4

---
+++ AOC04a/src/main.rs
@@ -0,0 +1,30 @@
+use regex::Regex;
+
+fn main() {
+    let inputs = include_str!("../input").lines();
+    let mut sum = 0;
+    for input in inputs {
+        let (_, inp) = input.split_once(':').unwrap();
+        let (a, b) = inp.split_once("|").unwrap();
+        let winnings: Vec<usize> = a
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect();
+        let numbers: Vec<usize> = b
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect();
+        let mut i = 0;
+        for winning in winnings {
+            if numbers.contains(&winning) {
+                if i == 0 {
+                    i = 1;
+                } else {
+                    i = i * 2;
+                }
+            }
+        }
+        sum = sum + i;
+    }
+    println!("{}", sum);
+}
---
+++ AOC04b/src/main.rs
@@ -0,0 +1,29 @@
+use regex::Regex;
+
+fn main() {
+    let inputs: Vec<&str> = include_str!("../input").lines().collect();
+    let mut number_of_cards = vec![1; inputs.len()];
+    for i in 0..inputs.len() {
+        let (_, inp) = inputs[i].split_once(':').unwrap();
+        let (a, b) = inp.split_once("|").unwrap();
+        let winnings: Vec<usize> = a
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect();
+        let numbers: Vec<usize> = b
+            .split_whitespace()
+            .map(|c| c.parse::<usize>().unwrap())
+            .collect();
+        let mut n = 0;
+        for winning in winnings {
+            if numbers.contains(&winning) {
+                n = n + 1;
+            }
+        }
+        for j in i + 1..n + i + 1 {
+            number_of_cards[j] = number_of_cards[j] + number_of_cards[i]
+        }
+    }
+    let sum: usize = number_of_cards.iter().sum();
+    println!("{}", sum);
+}
